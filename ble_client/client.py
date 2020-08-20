8027d112b28ad283da7e5bf4a5d7e6786eb99778
Brian Lerner <bnlerner@gmail.com>
2020-08-20 11:56:38 -0700

    adding peripheral client code

--- ble_client/client.py
+++ ble_client/client.py
@@ -1,4 +1,32 @@
+import bluepy
+from bluepy.btle import DefaultDelegate, Peripheral
+
+class NotificationDelegate(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
+
+    def handleNotification(self, cHandle, data):
+        print("A notification was received: %s" %data)
 
 
 def main():
     print("hello world!")
+    
+    p = Peripheral("UUID", "public")
+
+    p.setDelegate( NotificationDelegate() )
+
+    # Setup to turn notifications on, e.g.
+    svc = p.getServiceByUUID("47491010-1011-537e-4f6c-d104768a1001")
+    ch = svc.getCharacteristics("uuid_char")[0]
+    #ch_notify = svc.getCharacteristics()[1]
+    p.writeCharacteristic(ch.valHandle + 1, "\x01\x00", withResponse=True)
+
+while True:
+    if p.waitForNotifications(1.0):
+        # handleNotification() was called
+        print("got notification")
+        continue
+
+    print("Waiting...")
+    # Perhaps do something else here
